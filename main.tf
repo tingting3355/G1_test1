6d1cb0ee2d89ce08ec373b971e368a2272bd6a82
tingting3355 <skysorahwi@gamil.com>
2024-03-19 16:22:37 +0900

    Create subnet_Igw

--- main.tf
+++ main.tf
@@ -15,7 +15,53 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "g1-vpc-01" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main"
+  }
 }
+
+# Create a Public Subnet
+resource "aws_subnet" "g1-pub-sub-01" {
+  vpc_id     = aws_vpc.g1-vpc-01.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "g1-pub-sub-01"
+  }
+}
+
+resource "aws_subnet" "g1-pri-dev-sub-01" {
+  vpc_id     = aws_vpc.g1-vpc-01.id
+  cidr_block = "10.0.10.0/24"
+  tags = {
+    Name = "g1-pri-dev-sub-01"
+  }
+}
+
+resource "aws_subnet" "g1-pri-db-sub-01" {
+  vpc_id     = aws_vpc.g1-vpc-01.id
+  cidr_block = "10.0.20.0/24"
+  tags = {
+    Name = "g1-pri-db-sub-01"
+  }
+}
+
+resource "aws_subnet" "g1-pri-dev-sub-02" {
+  vpc_id     = aws_vpc.g1-vpc-01.id
+  cidr_block = "10.0.30.0/24"
+  tags = {
+    Name = "g1-pri-dev-sub-02"
+  }
+}
+
+
+resource "aws_internet_gateway" "g1-pub-igw" {
+  vpc_id = aws_vpc.g1-vpc-01.id
+  tags = {
+    Name = "g1-pub-igw"
+  }
+}
+
+
 
 # # 개발환경
 # module "default_custome_vpc" {
